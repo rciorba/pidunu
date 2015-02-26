756bd5f7d75f865c9460c4f9929cf16220bcd38e
Radu Ciorba <radu@devrandom.ro>
2015-02-26 23:19:49 +0200

    adds debug output

--- pidunu.c
+++ pidunu.c
@@ -6,12 +6,12 @@
 #include <signal.h>
 #include <errno.h>
 
-/*
-Do we care about the return code of the subprocess?
-Do we need to do anything about stdout/stderr?
- */
-pid_t g_child_pid = 0;
+#define DEBUG 1
 
+#define debug_print(fmt, ...) \
+  do { if (DEBUG) printf(fmt, __VA_ARGS__); fflush(NULL);} while (0)
+
+pid_t g_child_pid = 0;
 
 static void sigint(int signo) {
   if (g_child_pid > 1) {
@@ -29,15 +29,15 @@ int pid_one(pid_t child_pid) {
   int status;
   pid_t pid;
   setup_signals(child_pid);
-  /* printf("%d\t pid_one\n", getpid()); */
+  debug_print("%d\t pid_one\n", getpid());
   while(1) {
     pid = wait(&status);
     if (pid == child_pid){
       // the spawned child just terminated
-      /* printf("child died: %d\n", pid); */
+      debug_print("child died: %d\n", pid);
       return 0;
     }
-    /* printf("died: %d\n", pid); */
+    /* debug_print("died: %d\n", pid); */
   }
 }
 
@@ -46,27 +46,23 @@ void execute(int argc, char** argv) {
     return;
   char* cmd = argv[1];
   char** args = argv+1;
-  /* printf("%d\t %s ", getpid(), cmd); */
-  /* for (int i=0; i<argc-2; i++) { */
-  /*   printf("%s ", args[i]); */
-  /* } */
-  /* printf("\n%d\t ====\n", getpid()); */
+  debug_print("%d\t %s ", getpid(), cmd);
   if (execv(cmd, args) == -1) {
     fprintf(stderr, "failed to exec %s, errno is %d\n", cmd, errno);
   }
 }
 
 int main(int argc, char** argv) {
-  /* printf("%d\t main\n", getpid()); */
+  debug_print("%d\t main\n", getpid());
   pid_t pid = fork();
   if (pid<0) {
     fprintf(stderr, "failed to fork, errno: %d\n", errno);
     return -1;
   } else if (pid > 0) {
-    /* printf("%d\t pid unu\n", getpid()); */
+    debug_print("%d\t pid unu\n", getpid());
     return pid_one(pid);
   } else if (pid == 0) {
-    /* printf("%d\t pid 0\n", getpid()); */
+    debug_print("%d\t pid 0\n", getpid());
     execute(argc, argv);
     return -128; // program should have called exec and we should never get here
   }
