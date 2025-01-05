396038d1d55bf9bde8194544a0eec14393354c31
Radu Ciorba <radu.ciorba@hotjar.com>
2025-01-05 23:18:49 +0100

    update tests dependencies
    Upgrade to docker 7.1 & pytest 8.3

--- test/test_term.py
+++ test/test_term.py
@@ -10,14 +10,18 @@ import docker
 import pytest
 
 
+CONTAINER_PYTHON_PATH = "/usr/local/bin/python"
+
+
 class StreamHandler(object):
     def __init__(self, client, container):
         self.client = client
         self.container = container
-        self.stream = client.attach(container, stdout=True, stderr=True, logs=True, stream=True)
+        self.stream = container.attach(stdout=True, stderr=True, logs=True, stream=True)
 
     def wait(self, regexp=None):
         for line in self.stream:
+            line = line.decode('utf8')
             if regexp is not None and regexp.match(line):
                 break
 
@@ -25,10 +29,11 @@ class StreamHandler(object):
         """Parse the log lines and split by pid.
         Re-fetches the logs to avoid mixed lines in the stream.
         """
-        logs = self.client.attach(
-            self.container, stdout=True, stderr=True, logs=True, stream=False)
+        logs = self.container.attach(
+            stdout=True, stderr=True, logs=True, stream=False)
         logs_by_pid = defaultdict(list)
         for line in logs.splitlines():
+            line = line.decode('utf8')
             pid, msg = line.split(":", 1)
             logs_by_pid[int(pid)].append(msg)
         return dict(logs_by_pid)
@@ -37,22 +42,23 @@ class StreamHandler(object):
 @pytest.fixture
 def container_fixture(request):
     docker_url = os.getenv('DOCKER_URL', 'unix://var/run/docker.sock')
-    docker_image = os.getenv('DOCKER_IMAGE', 'ubuntu:14.04')
+    docker_image = os.getenv('DOCKER_IMAGE', 'python:alpine')
     bind_mount = os.getenv('BIND_MOUNT_PATH', os.getcwd())
-    test_bin = '/code/pidunu_dbg /usr/bin/python3 /code/test/bin/{}.py'.format(
-        request.function.__name__.replace("test_", "", 1)
-    )
-    client = docker.Client(base_url=docker_url, version='auto')
-    container = client.create_container(
+    func_name = request.function.__name__.replace("test_", "", 1)
+    test_bin = f'/code/pidunu_dbg {CONTAINER_PYTHON_PATH} /code/test/bin/{func_name}.py'
+    client = docker.DockerClient(base_url=docker_url, version='auto')
+    container = client.containers.create(
         image=docker_image,
-        volumes=['/code/'],
+        detach=True,
+        # volumes=['/code/'],
+        volumes = {bind_mount: {'bind': '/code/', 'mode': 'ro'}},
         command=test_bin,
         name="snowflake",
     )
     request.addfinalizer(
-        lambda: client.remove_container(container.get("Id"), force=True))
-    binds = {bind_mount: {'bind': '/code/', 'ro': True}}
-    client.start(container.get("Id"), binds=binds)
+        lambda: container.remove(force=True)
+    )
+    container.start()
     return client, container
 
 
@@ -67,8 +73,8 @@ def test_sigterm(container_fixture):
     """
     client, container = container_fixture
     stream = StreamHandler(client, container)
-    stream.wait(re.compile("\d+:term_py:start\n"))
-    client.stop(container.get("Id"), timeout=1)
+    stream.wait(re.compile(r"\d+:term_py:start\n"))
+    container.stop(timeout=1)
     stream.wait()
     logs = stream.split_logs_by_pid()
     p1_logs = logs[1]
@@ -83,7 +89,7 @@ def test_sigterm(container_fixture):
     ]
     expected[child_pid] = [
         "pidunu:fork=>0: {}".format(child_pid),
-        "pidunu:exec: /usr/bin/python3",
+        f"pidunu:exec: {CONTAINER_PYTHON_PATH}",
         "term_py:start",
         "term_py:SIGTERM",
     ]
@@ -116,7 +122,7 @@ def test_reaping(container_fixture):
     ]
     expected[child_pid] = [
         "pidunu:fork=>0: {}".format(child_pid),
-        "pidunu:exec: /usr/bin/python3",
+        f"pidunu:exec: {CONTAINER_PYTHON_PATH}",
     ]
     expected[child_pid+2] = [
         "child3 is done",
@@ -145,14 +151,14 @@ def test_signals(container_fixture):
     for name, signo in signals:
         expected_line = "signals:{}:{}".format(name, signo)
         expected_child_output.append(expected_line)
-        client.kill(container.get("Id"), signo)
+        container.kill(signo)
         stream.wait(re.compile(".*"+expected_line))
     logs = stream.split_logs_by_pid()
     p1_logs = logs[1]
     child_pid = int(p1_logs[1].rsplit(":", 1)[-1])
     expected = [
         "pidunu:fork=>0: {}".format(child_pid),
-        "pidunu:exec: /usr/bin/python3",
+        f"pidunu:exec: {CONTAINER_PYTHON_PATH}",
         "signals:start",
     ] + expected_child_output
     assert logs[child_pid] == expected
@@ -162,4 +168,4 @@ def test_return_code(container_fixture):
     """ Test return code is proxied correctly.
     """
     client, container = container_fixture
-    assert client.wait(container) == 42
+    assert container.wait().get("StatusCode") == 42
